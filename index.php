076db10eff92b1371506aebf61bb6bc5350f3d8d
Dipesh Deula <deuladipesh94@gmail.com>
2024-04-27 22:01:26 +0545

    index and logout updated

--- index.php
+++ index.php
@@ -3,20 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Result Management System</title>
+    <title>Deula Tech </title>
 </head>
 <body>
     <header>
-
+    DEULA<span>TECH<span>
     </header>
     <nav>
-        
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
     </nav>
     <main>
+        <section class="Home">
+            
+
+        </section>
+        <section class="About">
+
+        </section>
+        <section class="courses">
+
+        </section>
 
     </main>
     <footer>
-
+        
     </footer>
     
 </body>
