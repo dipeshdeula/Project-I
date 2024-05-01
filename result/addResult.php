0cf288fef5bed70d9de3f2dcdc4ddba469fef14c
Dipesh Deula <deuladipesh94@gmail.com>
2024-05-01 22:55:46 +0545

    result frontend updated

---
+++ result/addResult.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Result</title>
+</head>
+<body>
+    <header>
+        <nav class="nav">
+            <a href="addResult.php">Add Result</a>
+            <a href="manageResult.php">Manage Result</a>
+        </nav>
+    </header>
+    
+    <div class="main">
+        <div class="dashboard">
+            <?php include('../includes/leftbar.php');?>
+        </div>
+        <div class="container">
+            
+        </div>
+    </div>
+    
+</body>
+</html>
