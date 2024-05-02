85f6d4c6786777262889c27d0028ff6a89df94de
Dipesh Deula <deuladipesh94@gmail.com>
2024-05-02 07:12:22 +0545

    updation sujects details successfully

--- subject/deleteSubject.php
+++ subject/deleteSubject.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upda</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+require_once("../connection.php");
+
+$subCode = isset($_GET['id']) ? $_GET['id'] : null;
+
+if ($subCode) {
+    $query = "DELETE FROM tbl_subjects WHERE subCode = '$subCode'";
+    if (mysqli_query($conn, $query)) {
+        echo "<script>alert('Subject deleted successfully'); window.location='manageSubject.php';</script>";
+    } else {
+        echo "<script>alert('Error deleting subject: " . mysqli_error($conn) . "'); window.location='manageSubject.php';</script>";
+    }
+} else {
+    echo "<script>alert('No subject code provided'); window.location='manageSubject.php';</script>";
+}
+?>
